ec43ed33be46e15d755c6e0e4eb1e4b873e2c644
Salma Mahmoud <77031899+SaLmaaMahmoud@users.noreply.github.com>
2021-09-07 15:24:37 +0200

    Update main.h

--- FINAL_PROJ/FINAL_PROJ/main.h
+++ FINAL_PROJ/FINAL_PROJ/main.h
@@ -31,13 +31,13 @@ typedef	enum
 }	HEATER_STATE ;
 
 /* The Switches should be updated once every 20ms */
-#define SW_PERIOD				(20)
+#define SW_PERIOD		(20)
 /* The system should be updated once every 20ms */
-#define SYSTEM_PERIOD				(20)
+#define SYSTEM_PERIOD		(20)
 /* The SSD should blink every 1s in Setting mode */
-#define SSD_BLINK_PERIOD      			(100)
+#define SSD_BLINK_PERIOD    	(100)
 /* Temperature should be sensed once every 100ms */
-#define TEMPERATURE_period			(100)
+#define TEMPERATURE_period	(100)
 
 
 #endif /* MAIN_H_ */
